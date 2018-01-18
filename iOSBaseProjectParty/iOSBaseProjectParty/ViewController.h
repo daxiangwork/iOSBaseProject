deed879004ba7cc99bc25c588139c6cee58584ce
DaXiang <1295434840@qq.com>
2018-01-18 14:43:45 +0800

    Eric
    First Start

---
+++ iOSBaseProjectParty/iOSBaseProjectParty/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOSBaseProjectParty
+//
+//  Created by Eric on 2018/1/18.
+//  Copyright © 2018年 Eric. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ iOSBaseProjectParty/iOSBaseProjectParty/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  iOSBaseProjectParty
+//
+//  Created by Eric on 2018/1/18.
+//  Copyright © 2018年 Eric. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
