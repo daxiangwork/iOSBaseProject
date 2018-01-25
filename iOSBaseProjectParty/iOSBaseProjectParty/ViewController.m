b88585fc05dd355276fd50e578cd060d3a5af8da
DaXiang <1295434840@qq.com>
2018-01-25 10:37:08 +0800

    Test
    Test

--- iOSBaseProjectParty/iOSBaseProjectParty/ViewController.m
+++ iOSBaseProjectParty/iOSBaseProjectParty/ViewController.m
@@ -24,40 +24,38 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    NSLog(@"This is a new start !");
-    
-    VersionApi *api = [[VersionApi alloc] init];
-    [api startWithCompletionBlockWithSuccess:^(__kindof YTKBaseRequest * _Nonnull request) {
-        if (request.responseString) {
-            NetWorkingRespose *response = [NetWorkingRespose modelWithJSON:request.responseString];
-            if ([response.code isEqualToString:MBCode_SUCCESS]) {
-                VersionModel *model = [VersionModel modelWithDictionary:response.data];
-                NSLog(@"%@",model);
-            }else{
-                
-            }
-        }
-    } failure:^(__kindof YTKBaseRequest * _Nonnull request) {
-        NSLog(@"%@",request);
-    }];
-    
-    
-    //图片
-    NSData *photoData = UIImagePNGRepresentation([UIImage imageNamed:@"timg.jpeg"]);
-    NSString *filePath = [[EEFileManager sharedEEFileManager] createFilePath:@"R1200GSADV" fileType:EEFileType_Image extension:@".jpeg"];
-    BOOL ret = [[EEFileManager sharedEEFileManager] saveFile:filePath data:photoData];
-    
-    NSData *photo1Data = UIImagePNGRepresentation([UIImage imageNamed:@"tm.jpeg"]);
-    NSString *file1Path = [[EEFileManager sharedEEFileManager] createFilePath:@"R1200GSADV" fileType:EEFileType_Image extension:@".jpeg"];
-    BOOL ret1 = [[EEFileManager sharedEEFileManager] saveFile:file1Path data:photo1Data];
-
-    
-    NSData *imageddd = [[EEFileManager sharedEEFileManager] getFile:filePath];
-    UIImage *imaggg = [UIImage imageWithData:imageddd];
-    NSLog(@"====");
-    
-    BOOL ret2 = [[EEFileManager sharedEEFileManager] removeFile:file1Path];
-    BOOL ret3 = [[EEFileManager sharedEEFileManager] removeFile:filePath];
+//    NSLog(@"This is a new start !");
+//
+//    VersionApi *api = [[VersionApi alloc] init];
+//    [api startWithCompletionBlockWithSuccess:^(__kindof YTKBaseRequest * _Nonnull request) {
+//        if (request.responseString) {
+//            NetWorkingRespose *response = [NetWorkingRespose modelWithJSON:request.responseString];
+//            if ([response.code isEqualToString:MBCode_SUCCESS]) {
+//                VersionModel *model = [VersionModel modelWithDictionary:response.data];
+//                NSLog(@"%@",model);
+//            }else{
+//
+//            }
+//        }
+//    } failure:^(__kindof YTKBaseRequest * _Nonnull request) {
+//        NSLog(@"%@",request);
+//    }];
+//    //图片
+//    NSData *photoData = UIImagePNGRepresentation([UIImage imageNamed:@"timg.jpeg"]);
+//    NSString *filePath = [[EEFileManager sharedEEFileManager] createFilePath:@"R1200GSADV" fileType:EEFileType_Image extension:@".jpeg"];
+//    BOOL ret = [[EEFileManager sharedEEFileManager] saveFile:filePath data:photoData];
+//
+//    NSData *photo1Data = UIImagePNGRepresentation([UIImage imageNamed:@"tm.jpeg"]);
+//    NSString *file1Path = [[EEFileManager sharedEEFileManager] createFilePath:@"R1200GSADV" fileType:EEFileType_Image extension:@".jpeg"];
+//    BOOL ret1 = [[EEFileManager sharedEEFileManager] saveFile:file1Path data:photo1Data];
+//
+//
+//    NSData *imageddd = [[EEFileManager sharedEEFileManager] getFile:filePath];
+//    UIImage *imaggg = [UIImage imageWithData:imageddd];
+//    NSLog(@"====");
+//
+//    BOOL ret2 = [[EEFileManager sharedEEFileManager] removeFile:file1Path];
+//    BOOL ret3 = [[EEFileManager sharedEEFileManager] removeFile:filePath];
 }
 
 
